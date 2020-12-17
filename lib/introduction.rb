ebad674509862858f17e463d188b8fd5fe7c4c06
Ziad Zebib <zebibziad@gmail.com>
2020-12-17 02:32:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction_with_language_optional(name, language = "Ruby")
+def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
